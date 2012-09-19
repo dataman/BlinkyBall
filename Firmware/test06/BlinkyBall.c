94b5a989f38c9812495a176b14fb2429bf08faa5
Dataman <me@crjones.com>
2012-09-19 16:05:09 -0700

    Staging BlinkBall.C for diff changes

--- Firmware/test06/BlinkyBall.c
+++ Firmware/test06/BlinkyBall.c
@@ -1,19 +1,17 @@
-// BlinkyBall.c
-// firmware for BlinkyBall 
-// based on Dale Wheat's Tiny Cylon
+// tinyCylon2.c
+// revised firmware for tinyCylon LED scanner
+// written by dale wheat - 18 november 2008
+// based on behavior of original tinyCylon firmware
+
+// notes:
 
 // device = ATtiny13A
 // clock = 128 KHz internal RC oscillator
 // max ISP frequency ~20 KHz
 // brown-out detect = 1.8 V
 
-#define F_CPU 1000000UL  // 1 MHz
-//#define F_CPU 14.7456E6
-#include <util/delay.h>
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <avr/delay.h>
 
 // These registers are available on the ATtiny13A but not the original ATtiny13
 
@@ -30,28 +28,20 @@
 #define PRTIM0 1
 
 typedef enum {
-	MODE_0,  // Changes every per tilt, on for 2 seconds per tilt.
-	MODE_1,  // Changes every 5 seconds
-	MODE_2,  // Changes per tilt
+	MODE_0, // cylon scanner
+	MODE_0a, // single direction cylon scan
+	MODE_0b, // other direction cylon scan
+	MODE_1, // glowing pig eyes (2)
+	MODE_1a, // single glowing pig eye
+	MODE_1b, // single (random) glowing pig eye
+	MODE_2, // random blinking - multi
+	MODE_2a, // random blinking - single
+	MODE_2b, // random blinking - intermittant
+	MODE_2c, // random bblinking - really sparse
 	MODE_MAX // off
 } MODE;
+
 volatile MODE mode __attribute__ ((section (".noinit")));
-
-volatile unsigned int interrupt; // interrupt flag
-
-const unsigned char bbits[] = {
-    0b00000010,
-	0b00000011,
-	0b00001010,
-	0b00010010,
-	0b00001011,
-	0b00010011,
-	0b00011010
-};
-
-// PROTOTYPES
-void blink (unsigned char idelay);
-void sleep(unsigned char canwake);
 
 ///////////////////////////////////////////////////////////////////////////////
 // init() - initialize everything
@@ -64,7 +54,7 @@ void init(void) {
 	// turn off unused peripherals to save power
 
 	ACSR = 1<<ACD; // disable analog comparator
-	DIDR0 = 1<<ADC3D | 1<<ADC2D | 1<<ADC1D | 1<<ADC0D | 0<<AIN1D | 1<<AIN0D; // disable digital inputs
+	DIDR0 = 1<<ADC3D | 1<<ADC2D | 1<<ADC1D | 1<<ADC0D | 1<<AIN1D | 1<<AIN0D; // disable all digital inputs
 
 	// determine cause of device reset;  act accordingly
 
@@ -83,18 +73,15 @@ void init(void) {
 
 	// PORTB
 
-	//	PB5		1		PCINT5/-RESET/ADC0/dW		MODE advance pushbutton
+	//	PB0		5		MOSI/AIN0/OC0A/PCINT0		D1 output, active low
+	//	PB1		6		MISO/AIN1/OC0B/INT0/PCINT1	D2 output, active low
+	//	PB2		7		SCK/ADC1/T0/PCINT2			D3 output, active low
 	//	PB3		2		PCINT3/CLKI/ADC3			D4 output, active low
 	//	PB4		3		PCINT4/ADC2					D5 output, active low
-    //  GND     4       GROUND
+	//	PB5		1		PCINT5/-RESET/ADC0/dW		MODE advance pushbutton
 
-	//	PB0		5		MOSI/AIN0/OC0A/PCINT0		D1 output, active low
-	//	PB1		6		MISO/AIN1/OC0B/INT0/PCINT1	D2 input, tilt
- 	//	PB2		7		SCK/ADC1/T0/PCINT2			D3 output, n/c
-	//  VCC     8       POWER
-
-	PORTB = 0<<PORTB5 | 1<<PORTB4 | 1<<PORTB3 | 1<<PORTB2 | 0<<PORTB1 | 1<<PORTB0;
-	DDRB = 0<<DDB5 | 1<<DDB4 | 1<<DDB3 | 1<<DDB2 | 0<<DDB1 | 1<<DDB0;
+	PORTB = 0<<PORTB5 | 1<<PORTB4 | 1<<PORTB3 | 1<<PORTB2 | 1<<PORTB1 | 1<<PORTB0;
+	DDRB = 0<<DDB5 | 1<<DDB4 | 1<<DDB3 | 1<<DDB2 | 1<<DDB1 | 1<<DDB0;
 
 	// initialize ATtiny13 timer/counter
 
@@ -121,71 +108,230 @@ void delay(unsigned char n) {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+// pseudorandom number generator
+///////////////////////////////////////////////////////////////////////////////
+
+unsigned int prand(void) {
+
+    static unsigned int prand_value = 0xDA1E; // randomly seeded ;)
+    
+    prand_value = (prand_value >> 1) ^ (-(prand_value & 1) & 0xd001);
+
+    return prand_value;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// cylon() - simulate cylon scanner
+///////////////////////////////////////////////////////////////////////////////
+
+#define CYLON_SCAN_DELAY 30
+
+void cylon(unsigned char cylon_style) {
+
+	const unsigned char cylon_bits[] = {
+		0b00001110, // 1 & 5
+		0b00011110, // l
+		0b00011100, // 1 & 2
+		0b00011101, // 2
+		0b00011001, // 2 & 3
+		0b00011011, // 3
+		0b00010011, // 3 & 4
+		0b00010111, // 4
+		0b00000111, // 4 & 5
+		0b00001111  // 5
+	};
+	unsigned char i; // array iterator
+
+	while(1) {
+
+		if(cylon_style == 0) {
+
+			// traditional (back & forth) cylon scanner
+
+			for(i = 1; i < sizeof(cylon_bits); i++) {
+				PORTB = cylon_bits[i];
+				delay(CYLON_SCAN_DELAY);
+			}
+
+			for(i = sizeof(cylon_bits) - 2; i > 1; i--) {
+				PORTB = cylon_bits[i];
+				delay(CYLON_SCAN_DELAY);
+			}
+
+		} else if(cylon_style == 1) {
+
+			// single direction scan
+
+			for(i = 0; i < sizeof(cylon_bits); i++) {
+				PORTB = cylon_bits[i];
+				delay(CYLON_SCAN_DELAY);
+			}
+
+
+		} else if(cylon_style == 2) {
+
+			// other direction scan
+
+			for(i = sizeof(cylon_bits); i > 0; i--) {
+				PORTB = cylon_bits[i - 1];
+				delay(CYLON_SCAN_DELAY);
+			}
+		}
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// pig_eyes() - glowing pig eyes are scary
+///////////////////////////////////////////////////////////////////////////////
+
+void pig_eyes(unsigned char n) {
+
+	unsigned char leds_on, leds_off = 0b00011111;
+	unsigned char pwm_counter, pwm_value;
+
+	while(1) {
+	
+		if(n == 1) {
+			leds_on = 0b00011011; // one eye
+		} else if(n == 2) {
+			leds_on = 0b00001110; // 2 eyes
+		} else {
+			leds_on = 0b00011111 ^ (1 << (prand() % 5)); // single (random) eye
+		}
+
+		// ramp up...
+
+		for(pwm_value = 1; pwm_value < 128; pwm_value++) {
+			for(pwm_counter = 0; pwm_counter < 128; pwm_counter++) {
+				if(pwm_value > pwm_counter) {
+					PORTB = leds_on;
+				} else {
+					PORTB = leds_off;
+				}
+			}
+		}
+
+		// ... & ramp back down again
+
+		for(pwm_value = 127; pwm_value > 0; pwm_value--) {
+			for(pwm_counter = 0; pwm_counter < 128; pwm_counter++) {
+				if(pwm_value > pwm_counter) {
+					PORTB = leds_on;
+				} else {
+					PORTB = leds_off;
+				}
+			}
+		}
+
+		delay(250); // scary dark time
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// random() - random blinking
+///////////////////////////////////////////////////////////////////////////////
+
+void random(unsigned char n) {
+
+	while(1) {
+
+		if(n == 0) {
+
+			// light up a random number of LEDs
+			PORTB = prand() & 0x1F;
+
+		} else if(n == 1) {
+
+			// light up a single LED every time
+			PORTB = 0b00011111 ^ (1 << (prand() % 5));
+
+		} else {
+
+			if((prand() & n) == n) {
+	
+				// light up an LED (maybe)
+				PORTB = 0b00011111 ^ (1 << (prand() % 5));
+
+			} else {
+
+				// no LEDs on (maybe not)
+				PORTB = 0b00011111;
+			}
+		}
+
+		delay(50);
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
 // main() - main program function
 ///////////////////////////////////////////////////////////////////////////////
-int main(void) {
+
+void main(void) {
+
 	switch(mode) {
-	case MODE_0: 
-		blink(25);
-		break;
-	case MODE_1:
-		blink(100);
-		break;
-	case MODE_2:
-		blink(255);
-		break;
-	case MODE_MAX:
-		sleep(0);
-	}
-}
 
-///////////////////////////////////////////////////////////////////////////////
-// blink(delay) - blinks with interspacing delay
-///////////////////////////////////////////////////////////////////////////////
-void blink (unsigned char idelay) {
- interrupt = 0;
- unsigned char i=sizeof(bbits);
- while (1) {
-  if (++i >= sizeof(bbits)) i=0;
-  PORTB = bbits[i];
-  _delay_ms(idelay);
-  while (!interrupt) sleep(1);
-  interrupt--;
- }
-}
+		case MODE_0: // traditional (back & forth) cylon scanner
+			cylon(0);
+			break;
 
-///////////////////////////////////////////////////////////////////////////////
-// sleep(canwake) - 1/0 Enables or disables can wake on int0
-///////////////////////////////////////////////////////////////////////////////
-void sleep(unsigned char canwake) {
-	PORTB = 0b00011011; // all LEDs off
-	// deepest sleep mode
-	cli(); // disable interrupts
-	PRR = 1<<PRTIM0 | 1<<PRADC; // power down timer/counter0 & ADC
-	BODCR = 1<<BODS | 1<<BODSE; // enable BOD disable during sleep, step 1
-	BODCR = 1<<BODS | 0<<BODSE; // step 2
-	MCUCR = 1<<PUD | 1<<SE | 1<<SM1 | 0<<SM0 | 0<<ISC01 | 0<<ISC00; // select "power down" mode
-	if (canwake) {
-     // Enable tilt switch interrupt
-	 MCUCR = 1 << ISC01;  //set INT0 as falling edge trigger     
-     GIMSK = 1 << INT0;   //enable INTO in global interrupt mask
-	 sei(); // enable global interrupts
+		case MODE_0a: // single direction cylon scanner
+			cylon(1);
+			break;
+
+		case MODE_0b: // other direction cylon scanner
+			cylon(2);
+			break;
+
+		case MODE_1: // 2 glowing pig eyes
+			pig_eyes(2);
+			break;
+
+		case MODE_1a: // single glowing pig eye
+			pig_eyes(1);
+			break;
+
+		case MODE_1b: // single (random) glowing pig eye
+			pig_eyes(0);
+			break;
+
+		case MODE_2: // random blinking - multi
+			random(0);
+			break;
+
+		case MODE_2a: // random blinking - single
+			random(1);
+			break;
+
+		case MODE_2b: // random blinking - intermittant
+			random(3);
+			break;
+
+		case MODE_2c: // random blinking - really sparse
+			random(15);
+			break;
+
+		case MODE_MAX: // off
+			PORTB = 0b00011111; // all LEDs off
+			while(1) {
+				// deepest sleep mode
+				cli(); // disable interrupts
+				PRR = 1<<PRTIM0 | 1<<PRADC; // power down timer/counter0 & ADC
+				BODCR = 1<<BODS | 1<<BODSE; // enable BOD disable during sleep, step 1
+				BODCR = 1<<BODS | 0<<BODSE; // step 2
+				MCUCR = 1<<PUD | 1<<SE | 1<<SM1 | 0<<SM0 | 0<<ISC01 | 0<<ISC00; // select "power down" mode
+				asm("sleep"); // go to sleep to save power
+			}
 	}
-	asm("sleep"); // go to sleep to save power
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 // timer/counter0 overflow interrupt handler
 ///////////////////////////////////////////////////////////////////////////////
-ISR(TIM0_OVF_vect) {
-	downcounter--; // decrement downcounter for delay functions
-}
 
-///////////////////////////////////////////////////////////////////////////////
-// INT0 interrupt vector, activated by INT0 tilt.
-///////////////////////////////////////////////////////////////////////////////
-ISR(INT0_vect) {
-	interrupt = 25;
+ISR(TIM0_OVF_vect) {
+
+	downcounter--; // decrement downcounter for delay functions
 }
 
 ///////////////////////////////////////////////////////////////////////////////
